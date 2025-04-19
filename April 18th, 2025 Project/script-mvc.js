f986fc8a6bb2d4b54a0c164d3ab3ff0461ed6efa
drong24 <daisyrong24@gmail.com>
2025-04-18 23:38:44 -0700

    added edit button and editTodo function

--- April 18th, 2025 Project/script-mvc.js
+++ April 18th, 2025 Project/script-mvc.js
@@ -32,13 +32,52 @@ const state = {
   
   // removes todo item
   function deleteTodo(id) {
-    const newToDos = state.todos.filter((todo) => {
+    const newTodos = state.todos.filter((todo) => {
         return todo.id != id;
     });
 
-    state.todos = newToDos;
+    state.todos = newTodos;
     renderView();
   }
+
+  // Toggles todo complete
+  function toggleTodo(id) {
+    const li = document.getElementById(id);
+
+    const todo = state.todos.find(function(todo) {
+        console.log(todo.id + " - " + id);
+        return todo.id === id;
+    });
+    todo.completed = !todo.completed;
+
+    if (todo.completed == true) {
+        li.classList.add("completed");
+    } else {
+        li.classList.remove("completed");
+    }
+    renderView;
+  }
+
+  // Edit todo content 
+  function editTodo(id) {
+    const li = document.getElementById(id);
+    const input = document.createElement("input");
+    const confirmBtn = document.createElement("button");
+    const cancelBtn = document.createElement("button");
+    const text = li.querySelector(".text-content").textContent;
+    
+    input.classList.add("input");
+    confirmBtn.classList.add("confirm-btn");
+    cancelBtn.classList.add("cancel-btn");
+
+    input.value = text;
+    confirmBtn.textContent = "Confirm";
+    cancelBtn.textContent = "Cancel";
+
+    li.innerHTML = '';
+    li.append(input, confirmBtn, cancelBtn);
+  }
+
 
   // View
   const todoInput = document.querySelector("#todo-input");
@@ -49,16 +88,20 @@ const state = {
     const li = document.createElement("li");
     const span = document.createElement("span");
     const deleteBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
     const checkbox = document.createElement("input");
 
-    deleteBtn.classList.add("delete-btn");
+    span.classList.add("text-content");
     checkbox.classList.add("checkbox");
+    deleteBtn.classList.add("delete-btn");
+    editBtn.classList.add("edit-btn");
 
     li.id = todo.id;
     checkbox.type = "checkbox";
     span.textContent = todo.title;
     deleteBtn.textContent = "Delete";
-    li.append(checkbox, span, deleteBtn);
+    editBtn.textContent = "Edit";
+    li.append(checkbox, span, deleteBtn, editBtn);
   
     return li;
   }
@@ -79,9 +122,16 @@ const state = {
   listContainer.addEventListener('click', (e) => {
     const li = e.target.parentElement;
     if (e.target.className === 'delete-btn') {
-        deleteTodo(li.id);
+        deleteTodo(Number(li.id));
     } else if (e.target.className === 'checkbox') {
-        console.log(li.id + " checkbox");
+        toggleTodo(Number(li.id));
+    } else if (e.target.className === 'edit-btn') {
+        console.log("edited!");
+        editTodo(Number(li.id));
+    } else if (e.target.className === 'confirm-btn') {
+
+    } else if (e.target.className === 'cancel-btn') {
+
     }
   });  
   
