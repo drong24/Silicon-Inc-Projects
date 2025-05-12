3885eef9fcf29c3472535b00d66e1e7b62a62260
drong24 <daisyrong24@gmail.com>
2025-05-12 15:42:47 -0700

    clicking on the movie title now navigates to mevie details

--- React Movie DB Project/movie-db/src/MovieCard.jsx
+++ React Movie DB Project/movie-db/src/MovieCard.jsx
@@ -9,7 +9,9 @@ export default function MovieCard(props) {
             <div className="movie-card-img">
                 <img src={`https://image.tmdb.org/t/p/w500${props.movie.poster_path}`} alt="" />
             </div>
-        <h3 className="movie-card-title">{props.movie.title}</h3>
+        <h3 className="movie-card-title">
+            <Link to="/moviedetails">{props.movie.title}</Link>
+        </h3>
         <div className="movie-card-info">
             <div className="movie-card-rating">
                 <i className="ion-md-star rating-icon"></i>
