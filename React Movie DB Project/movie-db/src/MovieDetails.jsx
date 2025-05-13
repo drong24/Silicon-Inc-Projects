0dc26fc408ae0464b779aaac7da7ddf5aa9aac54
drong24 <daisyrong24@gmail.com>
2025-05-12 22:40:45 -0700

    styled movie details page

--- React Movie DB Project/movie-db/src/MovieDetails.jsx
+++ React Movie DB Project/movie-db/src/MovieDetails.jsx
@@ -22,39 +22,60 @@ export default function MovieDetails() {
     return (
         <div className="movie_details">
             <div className="movie_details_img">
-                <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="movie img" />
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="movie img" />
             </div>
             <div className="details_container">
                 <h2>{movie.title}</h2>
                 <br />
+                <h3>Release Date</h3>
+                <p>{movie.release_date}</p>
                 <h3>Overview</h3>
                 <p>{movie.overview}</p>
                 <h3>Genres</h3>
-                <ul>
+                <ul className="movie_genre_list">
                 {
                     movie.genres.map((genre) => {
                         return (
-                        <li key={genre.id}>{genre.name}</li>
+                        <li className="genre_li" key={genre.id}>{genre.name}</li>
                         );
                     })
                 }
                 </ul>
-                <h3>Rating</h3>
-                <p>{movie.vote_average}</p>
+                <h3>Average Rating</h3>
+                <div className="movie_rating_avg">
+                    <i className="ion-md-star rating-icon"></i>
+                    <p>{movie.vote_average}</p>
+                </div>
+                <h3>Your Rating</h3>
+                <div>
+                    <select name="rating" className="movie_rating_select">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
+                    <button>RATE IT!</button>
+                </div>
                 <h3>Production Companies</h3>
-                <ul>
+                <ul className="movie_company_list">
                     {
                         movie.production_companies.map((company) => {
                             return (
-                            <li key={company.id}>
+                            <li className="company_li" key={company.id}>
                                 <img src={`https://image.tmdb.org/t/p/w500/${company.logo_path}`} alt="company img" />
                             </li>
                             );
                         })
                     }
                 </ul>
+                <Link to="/">To home</Link>
             </div> 
-            <Link to="/">To home</Link>
         </div>
     );
 }
